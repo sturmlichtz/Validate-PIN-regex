8ae04e0ec30603aabc41ec75f9d2088eff7d1036
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-22 11:37:04 +0530

    add regex pattern

--- ValidatePIN_regex.sh
+++ ValidatePIN_regex.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-echo "regex pattern to validate PIN Code"
+#!/bin/bash -x
+read -p "regex pattern to validate PIN Code :- " PIN
+pin_Pattern="[0-9]"
+if [[ $PIN =~ $pin_Pattern ]]
+then
+	echo "Valid PIN Code"
+else
+	echo "Invalid PIN Code"
+fi
